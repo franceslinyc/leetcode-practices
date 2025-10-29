a8646bd07b8fab2db8a57bf45a882cc663a0692b
Frances Lin <franceslinyc@gmail.com>
2025-10-29 11:19:30 -0700

    10/29

--- 125.valid-palindrome.py
+++ 125.valid-palindrome.py
@@ -10,19 +10,14 @@ class Solution:
         l, r = 0, len(s) - 1
 
         while l < r:
-            while l < r and not self.alpha_num(s[l]):
+            while l < r and not s[l].isalnum():
                 l += 1
-            while r > l and not self.alpha_num(s[r]):
+            while r > l and not s[r].isalnum():
                 r -= 1
             if s[l].lower() != s[r].lower():
                 return False
             l, r = l + 1, r - 1
         return True
-
-    def alpha_num(self, c):
-        return (ord('A') <= ord(c) <= ord('Z') or
-                ord('a') <= ord(c) <= ord('z') or
-                ord('0') <= ord(c) <= ord('9'))
 
 # @lc code=end
 
