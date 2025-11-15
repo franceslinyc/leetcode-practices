c2b0f09103bfeac2396616d4a3d86d501ba69c26
Frances Lin <franceslinyc@gmail.com>
2025-11-14 16:06:18 -0800

    11/14

---
+++ 141.linked-list-cycle.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=141 lang=python3
+#
+# [141] Linked List Cycle
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+
+        fast, slow = head, head
+
+        while fast and fast.next: # Take care of edge case and ensure that fast.next.next can be accessed, None or not
+
+            slow = slow.next
+
+            fast = fast.next.next
+
+            if slow == fast: 
+
+                return True
+            
+        return False
+        
+# @lc code=end
+
