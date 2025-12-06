81f20f543528032d921867d00ca1e3dd9293e325
Frances Lin <franceslinyc@gmail.com>
2025-12-06 13:26:09 -0800

    12/6 First DP, confusing still

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -13,7 +13,7 @@ class Solution:
         # if n <= 2: 
         #     return n
         
-        # dp = [0] * (n + 1)        # To store # of ways from dp[0] to dp[n]
+        # dp = [0] * (n + 1)        # Store # of ways from dp[0] to dp[n]
         # dp[1], dp[2] = 1, 2
         # for i in range(3, n + 1): 
         #     dp[i] = dp[i-1] + dp[i-2]
@@ -21,15 +21,15 @@ class Solution:
 
         # method 2 dp (space optimized)
 
-        one, two = 1, 1             # # of ways to reach current and previous step
+        one, two = 1, 1             # # of ways to reach current and previous step, e.g., dp[i], dp[i-1]
         for i in range(n - 1): 
-            tmp = one               # Store it to update two 
+            tmp = one               # Store as tmp to update two 
             one = one + two         # Similar to dp[i] = dp[i-1] + dp[i-2] 
             two = tmp               # Update two
         return one
 
         # method 3 math
- 
-        
+
+
 # @lc code=end
 
