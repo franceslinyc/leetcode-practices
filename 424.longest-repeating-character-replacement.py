762fec94c83b9cc28e41e4e1261802c69ed29ea5
Frances Lin <franceslinyc@gmail.com>
2025-12-10 20:42:59 -0800

    12/10 Pretty hard!

---
+++ 424.longest-repeating-character-replacement.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode id=424 lang=python3
+#
+# [424] Longest Repeating Character Replacement
+#
+
+# @lc code=start
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        this_set = set(s)
+
+        res = 0
+
+        for c in this_set: 
+
+            count = 0
+
+            l = 0
+
+            for r in range(len(s)): 
+
+                if s[r] == c: 
+
+                    count += 1
+            
+                while (r - l + 1) - count > k:  # Shrink window from left if too many replacements needed
+
+                    if s[l] == c: 
+
+                        count -= 1
+
+                    l += 1
+
+                res = max(res, r - l + 1)       # Otherwise the window is valid, i.e., (r - l + 1) - count <= k
+
+        return res
+
+
+# AABABBA
+
+# this_set = {'A', 'B'}
+# outer loop loop through each character in this_set
+# inner loop expands the window one character at a time, from left to right
+
+
+# @lc code=end
+
