a1cc72de8de2260bc783efcb4e2549b651b8ebdd
Frances Lin <franceslinyc@gmail.com>
2025-12-20 03:45:33 +0800

    LC 102 Day6 Solve

--- 102.binary-tree-level-order-traversal.py
+++ 102.binary-tree-level-order-traversal.py
@@ -29,9 +29,9 @@ class Solution:
 
             level = []
 
-            for i in range(len(q)): 
+            for i in range(len(q)): # Process each node in the current level
 
-                node = q.popleft() 
+                node = q.popleft()  # Pops exactly that many nodes
 
                 level.append(node.val) 
 
@@ -48,7 +48,33 @@ class Solution:
         return res
 
         # method 3 BFS
-        
-        
+
+#     3
+#    / \ 
+#   9  20
+#      / \     
+#     15 17
+#
+# First while q: 
+# node = 3
+# q = []
+# q = [9,20]
+# level = [3]
+# res = [[3]]
+#
+# Second while q: 
+# node = 9
+# q = [20]
+# q = [20]     # 9 has no children
+# level = [9]
+# node = 20    # since q is not empty
+# q = []
+# q = [15, 17] # 20 has children
+# level = [9,20]
+# res = [[3], [9,20]]
+#
+# ...
+
+
 # @lc code=end
 
