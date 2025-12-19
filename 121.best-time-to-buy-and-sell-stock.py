3c641dc528e2aedb1c94a3b01623a3c4445fc607
Frances Lin <franceslinyc@gmail.com>
2025-12-19 10:06:20 +0800

    LC 121 >1Month Solve

--- 121.best-time-to-buy-and-sell-stock.py
+++ 121.best-time-to-buy-and-sell-stock.py
@@ -8,7 +8,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
+        # method 1 two pointers
+        
         max_profit = 0
+
         l, r = 0, 1
 
         while r < len(prices): 
@@ -17,15 +20,16 @@ class Solution:
 
                 l = r
             
-            else: 
+            profit = prices[r] - prices[l]
 
-                profit = prices[r] - prices[l]
-
-                max_profit = max(max_profit, profit)
+            max_profit = max(max_profit, profit)
 
             r += 1
 
         return max_profit
-        
+    
+        # method 2 DP
+
+
 # @lc code=end
 
