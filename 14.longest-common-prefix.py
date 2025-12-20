968ddf56c27fbb8aac9af649d33035291219515d
Frances Lin <franceslinyc@gmail.com>
2025-12-21 03:25:57 +0800

    12/21 Day0

--- 14.longest-common-prefix.py
+++ 14.longest-common-prefix.py
@@ -10,9 +10,13 @@ class Solution:
 
         res = ""
 
-        for i in range(len(strs[0])): 
+        for i in range(len(strs[0])):         # i = 0, ..., 5 for "flower"
 
-            for s in strs: 
+            # for j in range(len(strs)):      # Just for understanding purpose
+
+            #     if i == len(strs[j]) or strs[0][i] != strs[j][i]: 
+       
+            for s in strs:                    # "flower","flow","flight"
 
                 if i == len(s) or strs[0][i] != s[i]: 
 
@@ -21,7 +25,20 @@ class Solution:
             res += strs[0][i]
 
         return res
-    
-        
+
+
+# Outer loop controls the character position
+# Inner loop checks for all strings at the same position 
+# i = 0
+# Check "f" = "f" = "f"
+# Add to res
+# i = 1
+# Check "l" = "l" = "l"
+# Add to res
+# i = 2
+# Check "o" = "o" != "i"
+# Do not add
+
+
 # @lc code=end
 
