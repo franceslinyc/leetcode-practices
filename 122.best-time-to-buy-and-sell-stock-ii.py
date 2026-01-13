312a72fd65febaecd2c84a0342fda0a7b09c9204
Frances Lin <franceslinyc@gmail.com>
2026-01-13 10:58:12 +0800

    Lc 122 >2 Weeks Attempt

--- 122.best-time-to-buy-and-sell-stock-ii.py
+++ 122.best-time-to-buy-and-sell-stock-ii.py
@@ -14,7 +14,7 @@ class Solution:
 
             if prices[i] - prices[i - 1] > 0: 
 
-                profit += (prices[i] - prices[i - 1]) 
+                profit += (prices[i] - prices[i - 1]) # Increment profit; Can sell and buy multiple times 
 
         return profit 
 
