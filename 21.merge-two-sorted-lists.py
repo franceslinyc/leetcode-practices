bc0ee6cef3735e9213a219643469a05193ec4414
Frances Lin <franceslinyc@gmail.com>
2026-01-30 19:53:35 -0800

    LC 21 >1 Week Attempt

--- 21.merge-two-sorted-lists.py
+++ 21.merge-two-sorted-lists.py
@@ -41,7 +41,7 @@ class Solution:
 
             node.next = list2
 
-        return dummy.next
+        return dummy.next 
         # dummy.next return [1,1,2,3,4,4]
         #  otherwise return [0,1,1,2,3,4,4]
 
