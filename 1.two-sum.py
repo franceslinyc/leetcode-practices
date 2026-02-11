e4e6563b3c533f0b186a33aaaa249d011f9e90fb
Frances Lin <franceslinyc@gmail.com>
2026-02-11 15:07:52 -0800

    LC 1 Reset to Day0 Solve but with some attempt

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -8,17 +8,29 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
-        this_map = {}
+        this_dict = {}
 
         for i, n in enumerate(nums): 
             
             diff = target - n 
 
-            if diff in this_map: 
+            if diff in this_dict: 
 
-                return [i, this_map[diff]]
+                return [i, this_dict[diff]]
             
-            this_map[n] = i
+            this_dict[n] = i      # Careful here! 
+
+        # this_dict = {}
+
+        # for i in range(len(nums)): 
+
+        #     diff = target - nums[i]
+
+        #     if diff in this_dict: 
+
+        #         return [i, this_dict[diff]]
+            
+        #     this_dict[nums[i]] = i
 
 
 # @lc code=end
