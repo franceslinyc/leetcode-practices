7e8a3d86b8a44492d9520abf19437103abbddd2d
Frances Lin <franceslinyc@gmail.com>
2026-02-13 13:17:41 -0800

    LC 125 Reset to Day0 Attempt

--- 125.valid-palindrome.py
+++ 125.valid-palindrome.py
@@ -7,6 +7,30 @@
 # @lc code=start
 class Solution:
     def isPalindrome(self, s: str) -> bool:  
+               
+        l, r = 0, len(s) - 1
+
+        while l < r:             # l < r for comparing mirrored characters 
+
+            while l < r and not s[l].isalnum(): # Move l forward untill it points to an alphanumeric character
+
+                l += 1
+
+            while r > l and not s[r].isalnum(): # Move r backward
+
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
+
+                return False
+            
+            l += 1
+            r -= 1
+
+        return True
+    
+
+        # Not recommended; Extra time and space
         
         # new_s = ""
 
@@ -28,18 +52,7 @@ class Solution:
         #     r -= 1
 
         # return True
-        
-        l, r = 0, len(s) - 1
 
-        while l < r:
-            while l < r and not s[l].isalnum():
-                l += 1
-            while r > l and not s[r].isalnum():
-                r -= 1
-            if s[l].lower() != s[r].lower():
-                return False
-            l, r = l + 1, r - 1
-        return True
 
 # @lc code=end
 
