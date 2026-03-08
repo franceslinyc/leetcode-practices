15487323f5f4d574842f699ba6aaac5b76bacc36
Frances Lin <franceslinyc@gmail.com>
2026-03-08 11:26:42 -0700

    LC 49 >3 Weeks Solve but with hints

--- 49.group-anagrams.py
+++ 49.group-anagrams.py
@@ -8,6 +8,8 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
 
+        # method 1 hash map: O(M*N) time, M: length of a string, N: #s of strings in strs; O(M*N) space for output list 
+        
         res = defaultdict(list) # Instead of {}, handle edge case where a dictionary key is accessed before it exists. 
                                 # Default dict with a list; Initialize res[tuple(count)] = [] automatically
         for s in strs: 
@@ -23,9 +25,20 @@ class Solution:
         return list(res.values())       # Otherwise, res.values() return a dict, not list
 
 
-# eat 
-# count = a b c d e f g ... t
-#       =[1 0 0 0 1 0 0 ... 1]        
+# Idea:
+# 
+# We use a hash map where the key is a tuple (of the 26-character count) and the value is 
+# a list of strings. For each string, we compute its character frequency and use that tuple 
+# as the key to group anagrams together. The solution is O(MN) time because we process N 
+# strings and scan up to M characters per string, and O(MN) space because all N strings 
+# are stored in the hashmap groups and each string can be up to M long in the worst case.
+#
+# Examples: 
+# 
+#        A B C D E ...
+# count [1 1 1 ...... ] for "abc"   
+# count [1 1 1 ...... ] for "bca"   
+# count [1 1 0 1..... ] for "abd"  
 
 
 # @lc code=end
