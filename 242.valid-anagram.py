289b40c6945448325ba062c8b78f9b52bdbea59f
Frances Lin <franceslinyc@gmail.com>
2026-03-08 10:56:19 -0700

    LC 242 >3 Weeks Solve

--- 242.valid-anagram.py
+++ 242.valid-anagram.py
@@ -8,6 +8,8 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         
+        # method 1 hash map: O(n + m) time; O(1) space
+
         if len(s) != len(t): 
 
             return False
@@ -22,7 +24,22 @@ class Solution:
             t_dict[t[i]] = t_dict.get(t[i], 0) + 1
         
         return s_dict == t_dict
-    
+
+        # # method 2 hash map (array): O(n + m) time; O(1) space
+
+        # if len(s) != len(t):
+        #     return False
+
+        # count = [0] * 26
+        # for i in range(len(s)):
+        #     count[ord(s[i]) - ord('a')] += 1
+        #     count[ord(t[i]) - ord('a')] -= 1
+
+        # for val in count:
+        #     if val != 0:
+        #         return False
+        # return True
+
         
 # @lc code=end
 
