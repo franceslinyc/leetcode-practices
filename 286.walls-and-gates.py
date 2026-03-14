9968b191114bd0ff7450f122695146069a1a7027
Frances Lin <franceslinyc@gmail.com>
2026-03-14 12:55:01 -0700

    LC 286 Day1 Attempt

--- 286.walls-and-gates.py
+++ 286.walls-and-gates.py
@@ -13,15 +13,57 @@ class Solution:
         
         # method 1: BFS: time O((M*N)^2); space O(M*N)
 
-        # method 2: Multi Source BFS: time O(M*N); space O(M*N)
+        # # method 2: Multi Source BFS: time O(M*N); space O(M*N)
+
+        # ROWS, COLS = len(rooms), len(rooms[0])
+
+        # directions = [[1,0], [-1,0], [0,1], [0,-1]]
+
+        # INF = 2147483647
+
+        # # Put all gates in the queue
+
+        # q = deque()
+
+        # for r in range(ROWS):
+
+        #     for c in range(COLS):
+
+        #         if rooms[r][c] == 0:
+
+        #             q.append((r, c))
+
+        # # Run BFS from all gates
+
+        # while q:
+
+        #     row, col = q.popleft()
+
+        #     for dr, dc in directions:
+
+        #         nr, nc = row + dr, col + dc
+
+        #         if (0 <= nr < ROWS and
+
+        #             0 <= nc < COLS and
+
+        #             rooms[nr][nc] == INF):
+
+        #             rooms[nr][nc] = rooms[row][col] + 1  # Parent distance propagation
+        #                                                  # Initially rooms[row][col] = 0 because it is a gate! 
+
+        #             q.append((nr, nc))
+
+
+        # method 2: Multi Source BFS; Level BFS: time O(M*N); space O(M*N) 
 
         ROWS, COLS = len(rooms), len(rooms[0])
 
-        directions = [[1,0], [-1,0], [0,1], [0,-1]]
-
-        #visit = set()
+        direction = [[1,0], [-1,0], [0,1], [0,-1]]
 
         INF = 2147483647
+
+        #visit = set() # Use it if grid cannot be modified 
 
         # Put all gates in the queue
 
@@ -33,82 +75,60 @@ class Solution:
 
                 if rooms[r][c] == 0:
 
-                    q.append((r, c))
+                    q.append([r, c])
 
-                    # visit.add((r, c))
+                    #visit.add((r, c))
 
         # Run BFS from all gates
 
-        while q:
+        distance = 0
 
-            row, col = q.popleft()
+        while q:                          # Process all levels 
 
-            for dr, dc in directions:
+            distance += 1
 
-                nr, nc = row + dr, col + dc
+            for i in range(len(q)):       # Process ALL nodes in the current level
 
-                if (0 <= nr < ROWS and
+                row, col = q.popleft()
 
-                    0 <= nc < COLS and
+                #rooms[row][col] = dist
 
-                    rooms[nr][nc] == INF):
+                for dr, dc in direction: 
 
-                    rooms[nr][nc] = rooms[row][col] + 1  
+                    nr, nc = row + dr, col + dc
 
-                    q.append((nr, nc))
-
-
-        # # method 2: Multi Source BFS: time O(M*N); space O(M*N) 
-
-        # ROWS, COLS = len(rooms), len(rooms[0])
-
-        # direction = [[1,0], [-1,0], [0,1], [0,-1]]
-
-        # visit = set() # Need this for level BFS
-
-        # q = deque()
-
-        # for r in range(ROWS):
-
-        #     for c in range(COLS):
-
-        #         if rooms[r][c] == 0:
-
-        #             q.append([r, c])
-
-        #             visit.add((r, c))
-
-        # dist = 0
-
-        # while q:
-
-        #     for i in range(len(q)):
-
-        #         row, col = q.popleft()
-
-        #         rooms[row][col] = dist
-
-        #         for dr, dc in direction: 
-
-        #             nr, nc = row + dr, col + dc
-
-        #             if (0 <= nr < ROWS and 
+                    if (0 <= nr < ROWS and 
                     
-        #                 0 <= nc < COLS and 
+                        0 <= nc < COLS and 
 
-        #                 rooms[nr][nc] != -1 and 
+                        rooms[nr][nc] == INF     # rooms[nr][nc] == INF already mean unvisited! 
 
-        #                 (nr, nc) not in visit
-        #             ): 
+                        #rooms[nr][nc] != -1 and 
 
-        #                 q.append((nr, nc))
+                        #(nr, nc) not in visit
+                    ): 
+                        
+                        rooms[nr][nc] = distance
 
-        #                 visit.add((nr, nc))
+                        q.append((nr, nc))
 
-        #     dist += 1
+                        #visit.add((nr, nc))
 
 
         # method 3: DFS or Backtracking but is slow because this approach reexplores the same areas again and again
+
+
+# Multi-source BFS:
+#
+# Start BFS from all source nodes at the same time (i.e., all gates with value 0). 
+# Because BFS expands outward in increasing distance order, the first time a cell is reached 
+# it is guaranteed to be from the closest source.
+#
+# Level BFS:
+#
+# Process the queue layer by layer, where each layer represents nodes at the same distance from 
+# the start. After finishing one layer (for i in range(len(q))), increment the distance counter 
+# to represent moving one step further in the graph/grid.
 
 
 # @lc code=end
