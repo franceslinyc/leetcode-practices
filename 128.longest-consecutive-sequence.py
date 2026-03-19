b13d471a2d1c178350987f6a7fdeeb2e3c2d5d94
Frances Lin <franceslinyc@gmail.com>
2026-03-19 11:56:45 -0700

    LC 128 >1 Month Attempt

--- 128.longest-consecutive-sequence.py
+++ 128.longest-consecutive-sequence.py
@@ -33,16 +33,10 @@ class Solution:
         # method 2: hash map 
 
 
-# nums = [100,4,200,1,3,2]
-# nums = {100,4,200,1,3,2}
-# output = 4
-# Idea: 
-# 99 not there  -> 100 is a start -> but 101 is not -> Calculate max length
-# 4 is not a start
-# 199 not there -> 200 is a start -> but 201 is not -> Calculate max length
-# 0 not there   -> 1 is a start   -> 2 is -> Increment length -> 3 is -> 4 is -> but 5 is not -> Calculate max length
-# 3 is not a start
-# 2 is not a start 
+# We use a hash set to allow O(1) lookups and only begins expanding a sequence when the 
+# number is a start (i.e., (num - 1) is not in the set). From each starting point, 
+# it counts forward to measure the length of the consecutive sequence, ensuring each number 
+# is processed at most once. The algorithm runs in O(n) time with O(n) space due to the set.
 
 
 # @lc code=end
