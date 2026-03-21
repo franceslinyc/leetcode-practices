b548f786cb6ff6e27446607a6795714258e149f9
Frances Lin <franceslinyc@gmail.com>
2026-03-21 14:40:47 -0700

    LC 215 Day0 Small editing

--- 215.kth-largest-element-in-an-array.py
+++ 215.kth-largest-element-in-an-array.py
@@ -10,7 +10,7 @@ class Solution:
 
         # method 1: min heap: O(N log K) time; O(K) space
 
-        # ! Careful not to use heapify(nums) since this builds heap ds with size n (more work)
+        # !Careful not to use heapify(nums) since this builds heap ds with size n (more work)
 
         min_heap = []      
 
@@ -24,10 +24,12 @@ class Solution:
 
         return min_heap[0] # O(k) space        
 
+
         # method 2: quick select: O(N), O(N^2) time worst case; O(N) space
+        
 
         # method 3: quick select, optimal: O(N), O(N^2) time worst case; O(1) space
         
-        
+
 # @lc code=end
 
