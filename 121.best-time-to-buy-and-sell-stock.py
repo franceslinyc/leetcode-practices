665fd7d6a00db2083c26073589dca701c7264315
Frances Lin <franceslinyc@gmail.com>
2026-03-30 12:16:06 -0700

    LC 121 >1 Month Solve

--- 121.best-time-to-buy-and-sell-stock.py
+++ 121.best-time-to-buy-and-sell-stock.py
@@ -8,7 +8,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-        # method 1: two pointers 
+        # method 1: two pointers: O(n) time; O(1) space
         
         # Similar to LC 122; Look for single largest upward move
 
