b334e99c921e90622cc01e4f497fb9357c8a875f
Frances Lin <franceslinyc@gmail.com>
2026-04-03 15:39:50 -0700

    LC 70 Day0

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -8,28 +8,70 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
 
-        # # method 1 dp (bottom-up)
+        # # method 1 brute-force / recursive: O(2^n) time; O(n) space 
 
         # if n <= 2: 
+
         #     return n
-        
-        # dp = [0] * (n + 1)        # Store # of ways from dp[0] to dp[n]
-        # dp[1], dp[2] = 1, 2
+
+        # return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
+
+        # # method 2 dp (top-down): O(n) time; O(n) space
+        # # Optimized version of brute-force / recursive solution
+
+        # memo = {1:1, 2:2}
+
+        # def f(n): 
+
+        #     if n in memo: 
+
+        #         return memo[n]
+
+        #     memo[n] = f(n - 1) + f(n - 2)
+
+        #     return memo[n]
+
+        # return f(n)
+
+
+        # method 3 dp (bottom-up): O(n) time; O(n) space
+
+        if n <= 2: 
+
+            return n
+
+        dp = [0] * (n + 1)           # dp[i] = number of ways to reach step i
+
+        dp[1], dp[2] = 1, 2          # base cases
+
+        for i in range(3, n + 1):    # Start at 3 because we already know dp[1], dp[2]
+
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
+
+
+        # # method 4 dp (space optimized): O(n) time; O(1) space
+
+        # if n <= 2: 
+
+        #     return n
+
+        # one_back = 2
+
+        # two_back = 1
+
         # for i in range(3, n + 1): 
-        #     dp[i] = dp[i-1] + dp[i-2]
-        # return dp[n]
 
-        # method 2 dp (space optimized)
+        #     current = one_back + two_back
 
-        one, two = 1, 1             # # of ways to reach current and previous step, e.g., dp[i], dp[i-1]
-        for i in range(n - 1): 
-            tmp = one               # Store as tmp to update two 
-            one = one + two         # Similar to dp[i] = dp[i-1] + dp[i-2] 
-            two = tmp               # Update two
-        return one
+        #     two_back = one_back
 
-        # method 3 math
-
+        #     one_back = current
+        
+        # return one_back
+        
 
 # @lc code=end
 
