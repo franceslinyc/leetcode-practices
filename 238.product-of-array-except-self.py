c2b027a92b8d2397a693bff782fe1616dc257cca
Frances Lin <franceslinyc@gmail.com>
2026-05-07 11:28:07 -0700

    LC 238 Day1 Solve but not optimal

--- 238.product-of-array-except-self.py
+++ 238.product-of-array-except-self.py
@@ -8,52 +8,50 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
 
-        # # method 1: Prefix & Suffix; O(n) time; O(n) space (O(n) + O(n) + O(n) for prefix, postfix, and res array)
+        # method 1: Prefix & Suffix; O(n) time; O(n) space (O(n) + O(n) + O(n) for prefix, postfix, and res array)
 
-        # n = len(nums)
+        res = [0] * len(nums)
 
-        # res = [0] * n
+        prefix, postfix = [0] * len(nums), [0] * len(nums)
 
-        # prefix, postfix = [0] * n, [0] * n
+        prefix[0], postfix[len(nums) - 1] = 1, 1
 
-        # prefix[0], postfix[n - 1] = 1, 1
+        for i in range(1, len(nums)):
 
-        # for i in range(1, n):
+            prefix[i] = prefix[i - 1] * nums[i - 1]
 
-        #     prefix[i] = prefix[i - 1] * nums[i - 1]
+        for i in range(len(nums) - 2, -1, -1):    # Backward; Build from right, but careful! range(start, end, ), start is inclusive. end is not.  
 
-        # for i in range(n - 2, -1, -1):    # Backward; Build from right 
+            postfix[i] = postfix[i + 1] * nums[i + 1] 
 
-        #     postfix[i] = postfix[i + 1] * nums[i + 1] 
+        for i in range(len(nums)):
 
-        # for i in range(n):
-
-        #     res[i] = prefix[i] * postfix[i]
-
-        # return res
-
-
-        # method 2: Prefix & Suffix (Optimal); O(n) time; O(1) extra space, O(n) space for output array
-        
-        res = [1] * len(nums)
-
-        prefix = 1
-
-        for i in range(len(nums)): 
-
-            res[i] = prefix 
-
-            prefix *= nums[i]     # Update prefix to include current element for next iteration 
-
-        postfix = 1
-
-        for i in range(len(nums) - 1, -1, -1): 
-
-            res[i] *= postfix
-            
-            postfix *= nums[i]    # Update postfix to include current element for next iteration 
+            res[i] = prefix[i] * postfix[i]
 
         return res
+
+
+        # # method 2: Prefix & Suffix (Optimal); O(n) time; O(1) extra space, O(n) space for output array
+        
+        # res = [1] * len(nums)
+
+        # prefix = 1
+
+        # for i in range(len(nums)): 
+
+        #     res[i] = prefix 
+
+        #     prefix *= nums[i]     # Update prefix to include current element for next iteration 
+
+        # postfix = 1
+
+        # for i in range(len(nums) - 1, -1, -1): 
+
+        #     res[i] *= postfix
+            
+        #     postfix *= nums[i]    # Update postfix to include current element for next iteration 
+
+        # return res
 
 
 # We use a prefix and postfix pass: First, fill the result with prefix products (left products), 
@@ -65,6 +63,13 @@ class Solution:
 # prefix = [1, 1, 2, 6]
 # postfix= [24,12,4, 1]
 # res    = [24,12,8, 6]
+
+# Some parts get computed many times. ASK: Can we precompute reusable partial products?
+# nums =   [1, 2, 3, 4]
+# 1: 2 * 3 * 4
+# 2: 1 * 3 * 4
+# 3: 1 * 2 * 4
+# 4: 1 * 2 * 3
 
 
 # @lc code=end
