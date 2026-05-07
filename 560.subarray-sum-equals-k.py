2d2297fc9f1ee3fbf8fd0d90f64f029c2fcb0075
Frances Lin <franceslinyc@gmail.com>
2026-05-07 11:28:22 -0700

    LC 560 Day1 Solve but slow

--- 560.subarray-sum-equals-k.py
+++ 560.subarray-sum-equals-k.py
@@ -14,13 +14,17 @@ class Solution:
 
         # for i in range(len(nums)):
 
-        #     sum = 0
+        #     current_prefix_sum = 0
 
+        #     # Loop through subarray from i to each j -> nums[i....j]
+            
         #     for j in range(i, len(nums)):
 
-        #         sum += nums[j]
+        #         current_prefix_sum += nums[j]
 
-        #         if sum == k:
+        #         # Increment res if the current prefix sum is equal to k
+                
+        #         if current_prefix_sum == k: 
 
         #             res += 1
 
@@ -31,27 +35,29 @@ class Solution:
 
         res = 0 
 
-        current_prefix_sum = 0 
-
         prefix_sum_map = {0:1} #{}
+
+        current = 0   # current_prefix_sum
 
         for num in nums: 
 
-            current_prefix_sum += num
+            current += num
 
-            diff = current_prefix_sum - k        # prefix[j] - prefix[i] = k <=> prefix[i] = prefix[j] - k
+            # Increment res if a subarray with sum k exists
 
-            if diff in prefix_sum_map: 
+            prev = current - k
 
-                res += prefix_sum_map[diff]
+            if prev in prefix_sum_map: 
 
-            prefix_sum_map[current_prefix_sum] = prefix_sum_map.get(current_prefix_sum, 0) + 1
+                res += prefix_sum_map[prev]
+
+            prefix_sum_map[current] = prefix_sum_map.get(current, 0) + 1
 
         return res
 
 
 # We use a running prefix sum and a hashmap to store the frequency of each prefix sum. 
-# For each element, we check how many times previous prefix sums (current prefix sum - k) 
+# For each element, we check how many times previous prefix sum (current prefix sum - k) 
 # has occured, which tells us how many subarrays ending at the current index sum to k. This
 # gives us O(N) time since we traverse the array once, and O(N) space since we store prefix
 # sums in the hashmap. 
@@ -65,23 +71,23 @@ class Solution:
 
 # index = 0, num = 1
 # current_prefix_sum = 1
-# need = 1 - k = 1 - 2 = -1 Not seen -> Do not update res
-# prefix_sum_map = {0:1, 1:1}
+# need = 1 - k = 1 - 2 = -1 Not seen in map-> Do not update res
+# prefix_sum_map = {0:1, 1:1} Always add current_prefix_sum:count to map
 
 # index = 1, num = -1
 # current_prefix_sum = 0
-# need = 0 - k = 0 - 2 = -2 Not seen -> Do not update res
-# prefix_sum_map = {0:2, 1:1}
+# need = 0 - k = 0 - 2 = -2 Not seen in map-> Do not update res
+# prefix_sum_map = {0:2, 1:1} Always add current_prefix_sum:count to map
 
 # index = 2, num = 1
 # current_prefix_sum = 1
-# need = 1 - k = 1 - 2 = -1 Not seen -> Do not update res
-# prefix_sum_map = {0:2, 1:2}
+# need = 1 - k = 1 - 2 = -1 Not seen in map-> Do not update res
+# prefix_sum_map = {0:2, 1:2} Always add current_prefix_sum:count to map
 
 # index = 3, num = 1
 # current_prefix_sum = 2
-# need = 2 - k = 2 - 2 = 0 Seen 2 times -> res += 2 -> res = 2
-# prefix_sum_map = {0:2, 1:2, 2:1}
+# need = 2 - k = 2 - 2 = 0 Seen 2 times in map -> Update res += 2 -> res = 2
+# prefix_sum_map = {0:2, 1:2, 2:1} Always add current_prefix_sum:count to map
 
 
 # @lc code=end
