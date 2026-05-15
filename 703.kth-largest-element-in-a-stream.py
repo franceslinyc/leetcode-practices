dd47cd2f5928461ae7dd023a06587f4cc94d0d26
Frances Lin <franceslinyc@gmail.com>
2026-05-15 11:29:33 -0700

    LC 703 >2 Months Attempt

--- 703.kth-largest-element-in-a-stream.py
+++ 703.kth-largest-element-in-a-stream.py
@@ -13,21 +13,21 @@ class KthLargest:
 
         self.min_heap = nums
 
-        heapq.heapify(self.min_heap)           # Rearranges list into a valid min heap (default)
+        heapify(self.min_heap)           # Rearranges list into a valid min heap (default)
 
         while len(self.min_heap) > k:          
 
-            heapq.heappop(self.min_heap)       # Remove smallest element to keep only k largest
+            heappop(self.min_heap)       # Remove smallest element to keep only k largest; heappop ALWAYS remove smallest element 
 
     def add(self, val: int) -> int:
 
         # Careful! Remember to use self. to access instance varaible
         
-        heapq.heappush(self.min_heap, val)     # Maintains heap property after insertion
+        heappush(self.min_heap, val)     # Maintain heap property after insertion; heappush Insert into heap while maintaining order 
 
         if len(self.min_heap) > self.k: 
 
-            heapq.heappop(self.min_heap)
+            heappop(self.min_heap)
 
         return self.min_heap[0]
 
@@ -37,3 +37,36 @@ class KthLargest:
 # param_1 = obj.add(val)
 # @lc code=end
 
+
+# import heapq
+# heapq.heapify
+# heapq.heappush Insert into heap while maintaining order 
+# heapq.heappop  ALWAYS remove smalles element 
+
+
+# e.g., 
+# ["KthLargest", "add", "add", "add", "add", "add"]
+# [[3, [4, 5, 8, 2]], [3], [5], [10], [9], [4]]
+
+# [4, 5, 8, 2]
+# [2, 4, 5, 8]
+# keep [4, 5, 8]
+# 3rd largest = 4
+
+# add 3
+# [4, 5, 8, 2, 3]
+# [2, 3, 4, 5, 8]
+# keep [4, 5, 8]
+# 3rd largest = 4
+
+# add 5
+# [4, 5, 8, 2, 3, 5]
+# [2, 3, 4, 5, 5, 8]
+# keep [5, 5, 8]
+# 3rd largetst = 5
+
+# add 10
+# [4, 5, 8, 2, 3, 5, 10]
+# [2, 3, 4, 5, 5, 8, 10]
+# keep [5, 8, 10]
+# 3rd largetst = 5
