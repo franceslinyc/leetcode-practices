102b8ad0831ab2edaab924bed6536dfc9251bddf
Frances Lin <franceslinyc@gmail.com>
2026-05-28 12:38:19 -0700

    LC 509 >1 Month Attempt

--- 509.fibonacci-number.py
+++ 509.fibonacci-number.py
@@ -10,13 +10,9 @@ class Solution:
 
         # # method 1 brute-force: O(2^n) time; O(n) space
 
-        # if n == 0: 
+        # if n <= 1: 
 
-        #     return 0
-
-        # if n == 1: 
-
-        #     return 1
+        #     return n
 
         # return self.fib(n - 1) + self.fib(n - 2) # O(n) space for recursion call
 
@@ -25,38 +21,63 @@ class Solution:
 
         # memo = {0:0, 1:1}
 
-        # def f(x): 
+        # def f(n): 
 
-        #     if x in memo: 
+        #     if n in memo: 
 
-        #         return memo[x]
+        #         return memo[n]
 
-        #     memo[x] = f(x - 1) + f(x - 2)
+        #     memo[n] = f(n - 1) + f(n - 2)
 
-        #     return memo[x]
+        #     return memo[n]
 
         # return f(n)
 
 
+        # memo = {} # Remove base cases
+
+        # def f(n): 
+
+        #     if n <= 1:     # Base case 
+
+        #         return n
+
+        #     if n in memo:  # If Subproblem in memo map
+
+        #         return memo[n]
+
+        #     memo[n] = f(n - 1) + f(n - 2)
+
+        #     return memo[n]
+
+        # return f(n)  
+
+
         # method 3 dp bottom-up: O(n) time; O(n) space
 
-        if n == 0: 
+        if n <= 1: 
 
-            return 0
+            return n
 
-        if n == 1: 
-
-            return 1        
-
-        dp = [0] * (n + 1)           # dp[i] = the ith Fibonacci number
+        dp = [0] * (n + 1)           # dp[i] = the ith Fibonacci number; n + 1 because we need to go to go from 0 to n inclusive.
         
-        dp[0], dp[1] = 0, 1
+        dp[0], dp[1] = 0, 1          # base cases
  
         for i in range(2, n + 1):    # Start at 2 because we already know dp[0], dp[1]
 
             dp[i] = dp[i - 1] + dp[i - 2]
  
         return dp[n] 
+
+
+# @lc code=end
+
+
+# F(0) = 0                     <- base cases
+# F(1) = 1                     <- base cases
+# F(2) = F(1) + F(0)
+# F(3) = F(2) + F(1) 
+# F(n) = F(n - 1) + F(n - 2)   <- formula 
 
 
 # method 1: O(2^n) time and O(n) space. 
@@ -70,7 +91,3 @@ class Solution:
 #        f(1) f(2)f(1)f(2)f(2) f(3)
 #                             / \
 #                          f(1) f(2)
-
-
-# @lc code=end
-
