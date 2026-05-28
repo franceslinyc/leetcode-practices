0a9d5482bd0be50e2be868bd5cd09423b9e4ce1b
Frances Lin <franceslinyc@gmail.com>
2026-05-28 12:38:25 -0700

    LC 70 >1 Month Attempt

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -43,7 +43,7 @@ class Solution:
 
         dp = [0] * (n + 1)           # dp[i] = number of ways to reach step i
 
-        dp[1], dp[2] = 1, 2          # base cases
+        dp[1], dp[2] = 1, 2          # base cases; dp[0] = 0 does not make sense
 
         for i in range(3, n + 1):    # Start at 3 because we already know dp[1], dp[2]
 
@@ -58,20 +58,32 @@ class Solution:
 
         #     return n
 
-        # one_back = 2
+        # two_step_back = 1         # <- dp[1]
 
-        # two_back = 1
+        # one_step_back = 2         # <- dp[2]
 
         # for i in range(3, n + 1): 
 
-        #     current = one_back + two_back
+        #     current = one_step_back + two_step_back      # <- dp[i] = dp[i-1] + dp[i-2]
 
-        #     two_back = one_back
+        #     two_step_back = one_step_back                # Old dp[i-1] becomes next iteration's dp[i-2]
 
-        #     one_back = current
+        #     one_step_back = current                      # Current dp[i] becomes next iter's dp[i-1]
         
-        # return one_back
+        # return one_step_back
         
 
 # @lc code=end
 
+
+# n = 1 -> F(1) = 1 way               <- base cases
+# n = 2 -> F(2) = 2 way               <- base cases
+# n = 3 -> F(3) = F(2) + F(1) = 3 way
+# n = 4 -> F(4) = F(3) + F(2) = 5 way
+#          F(n) = F(n - 1) + F(n - 2) <- formula 
+
+# F(3) = 3 way
+# {1+1+1, 1+2, 2+1} 
+
+# F(4) = 5 way
+# {1+1+1+1, 1+1+2, 1+2+1, 2+1+1, 2+2} 
