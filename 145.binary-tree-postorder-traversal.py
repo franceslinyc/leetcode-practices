03562cb60a6c3a0e3bbf9f6795b426d0ef891d42
Frances Lin <franceslinyc@gmail.com>
2026-06-04 11:40:55 -0700

    Add example

--- 145.binary-tree-postorder-traversal.py
+++ 145.binary-tree-postorder-traversal.py
@@ -39,3 +39,42 @@ class Solution:
         
 # @lc code=end
 
+
+# Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+# Output: [4,6,7,5,2,9,8,3,1]
+
+#         1
+#        / \
+#       2   3
+#      / \   \
+#     4   5   8
+#        / \ /
+#       6  7 9
+
+# dfs(1)
+#   dfs(2)
+#     dfs(4)
+#       dfs(None) ✓ base
+#       dfs(None) ✓ base
+#     ← append(4),  res=[4]
+#     dfs(5)
+#       dfs(6)
+#         dfs(None) ✓ base
+#         dfs(None) ✓ base
+#       ← append(6),  res=[4,6]
+#       dfs(7)
+#         dfs(None) ✓ base
+#         dfs(None) ✓ base
+#       ← append(7),  res=[4,6,7]
+#     ← append(5),  res=[4,6,7,5]
+#   ← append(2),    res=[4,6,7,5,2]
+#   dfs(3)
+#     dfs(None) ✓ base
+#     dfs(8)
+#       dfs(9)
+#         dfs(None) ✓ base
+#         dfs(None) ✓ base
+#       ← append(9),  res=[4,6,7,5,2,9]
+#     ← append(8),    res=[4,6,7,5,2,9,8]
+#   ← append(3),    res=[4,6,7,5,2,9,8,3]
+# ← append(1),      res=[4,6,7,5,2,9,8,3,1]
