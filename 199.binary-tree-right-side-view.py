457c895121eaa77da50c32e197c333e01b29cb39
Frances Lin <franceslinyc@gmail.com>
2026-07-30 16:32:46 -0700

    LC 199 >3Month Solve

--- 199.binary-tree-right-side-view.py
+++ 199.binary-tree-right-side-view.py
@@ -14,40 +14,7 @@
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
 
-        # method 1 bfs: time O(n), space O(n), where n: # of nodes; balanced BFS -> time O(log n)
-
-        if not root: 
-
-            return []
-        
-        q = deque([root])
-
-        res = []
-
-        while q: 
-
-            rightside = None         # vs LC 102 level = [] 
-
-            for i in range(len(q)): 
-
-                node = q.popleft()
-
-                rightside = node.val # Keep overwriting; vs LC 102 level.append(node.val) store the entire level
-
-                if node.left: 
-
-                    q.append(node.left)
-                
-                if node.right: 
-
-                    q.append(node.right)
-
-            res.append(rightside)    # Safer to add instead of adding node.val directly 
-            
-        return res
-
-    
-        # # method 2 dfs
+        # # method 1 DFS: time O(n), space O(n), where n: # of nodes 
 
         # res = []
 
@@ -78,20 +45,37 @@ class Solution:
         # return res 
 
 
-# BFS: 
-# 1. Build tree level by level
-# 2. Use queue to find the path
-# 3. Work on the concept of FIFO
-#
-# Idea: 
-# This problem can be solved using Breadth-First Search with a queue.
-# We process nodes level by level.
-# At each iteration, we use the queue size to determine how many nodes belong to the current level.
-# We process all nodes in that level and add their children. 
-# The last node processed in that level is the rightmost node,
-# so we add its value to the result list.
-# Then we repeat until the queue is empty.
-# Time complexity is O(n), and space complexity is O(n).
+        # method 2 BFS: time O(n), space O(n), where n: # of nodes; balanced BFS -> time O(log n)
+
+        if not root: 
+
+            return []
+        
+        q = deque([root])
+
+        res = []
+
+        while q: 
+
+            rightside = None         # vs LC 102 level = [] 
+
+            for i in range(len(q)): 
+
+                node = q.popleft()
+
+                rightside = node.val # Keep overwriting; vs LC 102 level.append(node.val) store the entire level
+
+                if node.left: 
+
+                    q.append(node.left)
+                
+                if node.right: 
+
+                    q.append(node.right)
+
+            res.append(rightside)    # Safer to add instead of adding node.val directly 
+            
+        return res
 
 
 # @lc code=end
