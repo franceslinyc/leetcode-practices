a12f343035d31930e0bb52acc76d2ee52d57a70d
Frances Lin <franceslinyc@gmail.com>
2026-09-04 11:05:49 -0700

    LC 438 3Month Solve after solving LC 567

--- 438.find-all-anagrams-in-a-string.py
+++ 438.find-all-anagrams-in-a-string.py
@@ -79,15 +79,25 @@ class Solution:
 
             s_count[ord(s[r]) - ord('a')] += 1
 
-            if (r - l + 1) > len(p):      # Keep window size equal to len(p)
+            # if (r - l + 1) > len(p):      # Keep window size equal to len(p)
+
+            #     s_count[ord(s[l]) - ord('a')] -= 1
+
+            #     l += 1            
+        
+            # if s_count == p_count: 
+
+            #     res.append(l)
+
+            if (r - l + 1) == len(p): 
+
+                if s_count == p_count: 
+
+                    res.append(l)
 
                 s_count[ord(s[l]) - ord('a')] -= 1
 
-                l += 1            
-        
-            if s_count == p_count: 
-
-                res.append(l)
+                l += 1
 
         return res
 
